fcf849313fed13cbad4a93b649b95f70ee599568
rxsweet <105535100+rxsweet@users.noreply.github.com>
2022-09-23 18:21:29 +0800

    Update sub_convert.py

--- utils/test/sub_convert.py
+++ utils/test/sub_convert.py
@@ -707,8 +707,8 @@ class sub_convert():
 if __name__ == '__main__':
     subscribe = 'https://raw.githubusercontent.com/rxsweet/fetchProxy/main/sub/rx64'
     output_path = './output.yml'
-
-    content = sub_convert.convert_remote(subscribe,'clash')
+    sub_convert.use_urlhost()
+    content = sub_convert.convert_remote(subscribe,'clash',sub_convert.use_urlhost())
 
     file = open(output_path, 'w', encoding= 'utf-8')
     file.write(content)
