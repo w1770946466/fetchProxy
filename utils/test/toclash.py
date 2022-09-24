8056465d699d3ba67d65c0fef955210dadf05ba6
rxsweet <105535100+rxsweet@users.noreply.github.com>
2022-09-24 09:16:50 +0800

    Update toclash.py

--- utils/test/toclash.py
+++ utils/test/toclash.py
@@ -2,11 +2,9 @@ from sub_convert import sub_convert
 
 if __name__ == '__main__':
     subscribe = 'https://raw.githubusercontent.com/rxsweet/fetchProxy/main/sub/rx64'
-    hosturl = 'https://pub-api-1.bianyuan.xyz'
     output_path = './output.yml'
 
-    content = sub_convert.convert_remote(subscribe,'clash',hosturl)
-
+    content = sub_convert.convert_remote(subscribe,'clash',sub_convert.use_urlhost())
     file = open(output_path, 'w', encoding= 'utf-8')
     file.write(content)
     file.close()
