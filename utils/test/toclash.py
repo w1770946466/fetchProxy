1e0af2bf32fc4a710546282e7fd9a8abfca344fd
rxsweet <105535100+rxsweet@users.noreply.github.com>
2022-09-24 09:46:08 +0800

    Update toclash.py

--- utils/test/toclash.py
+++ utils/test/toclash.py
@@ -1,11 +1,18 @@
+import os
 from sub_convert import sub_convert
 
-if __name__ == '__main__':
-    subscribe = 'https://raw.githubusercontent.com/rxsweet/fetchProxy/main/sub/rx64'
-    output_path = './output.yml'
+#文件位置
+input_source_file = './sub/literx'
+output_file = './sub/literxClash.yml'
 
-    content = sub_convert.convert_remote(subscribe,'clash',sub_convert.use_urlhost())
-    file = open(output_path, 'w', encoding= 'utf-8')
+if __name__ == '__main__':
+    #转换
+    input_check_file_path = os.path.abspath(input_source_file)  # 获取文件路径
+    print（str(input_check_file_path)+'\n'）
+    content = sub_convert.convert_remote(input_check_file_path,'clash',sub_convert.use_urlhost())   #转换
+    print(content)
+    #写入
+    file = open(output_file, 'w', encoding= 'utf-8')
     file.write(content)
     file.close()
-    print(f'Writing content to output.yml\n')
+    print(f'Writing content done！\n')
