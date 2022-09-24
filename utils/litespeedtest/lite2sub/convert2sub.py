fccd4aacc680d3b51463edeae16108b63325bc91
rxsweet <105535100+rxsweet@users.noreply.github.com>
2022-09-24 08:59:23 +0800

    Update convert2sub.py

--- utils/litespeedtest/lite2sub/convert2sub.py
+++ utils/litespeedtest/lite2sub/convert2sub.py
@@ -3,11 +3,10 @@ from sub_convert import sub_convert
 #文件位置
 input_source_file = './sub/literx'
 output_file = './sub/literxClash.yml'
-hosturl = 'https://pub-api-1.bianyuan.xyz'
 
 if __name__ == '__main__':
     #转换
-    content = sub_convert.convert_remote(subscribe,'clash',hosturl) 
+    content = sub_convert.convert_remote(input_source_file,'clash',sub_convert.use_urlhost()) 
     #写入
     file = open(output_file, 'w', encoding= 'utf-8')
     file.write(content)
