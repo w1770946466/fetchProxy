fdea6d55178da138a81c07cc985858a74c35c785
rxsweet <105535100+rxsweet@users.noreply.github.com>
2022-10-02 11:08:55 +0800

    Update check.py

--- utils/checkclash/check.py
+++ utils/checkclash/check.py
@@ -5,6 +5,7 @@ import json
 def check(alive, proxy, apiurl, sema, timeout, testurl):
     try:
         r = requests.get(url=apiurl + '/proxies/' + str(proxy['name']) + '/delay?url='+testurl+'&timeout=' + str(timeout), timeout=10)
+        print('text ='+r.text+'\n')
         response = json.loads(r.text)
         if response['delay'] > 0:
             alive.append(proxy)
