3a9e0e17fbad6db8dc3aba11529105597e7a7854
rxsweet <105535100+rxsweet@users.noreply.github.com>
2022-10-02 09:32:28 +0800

    Update main.py

--- utils/checkclash/main.py
+++ utils/checkclash/main.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
         for p in processes:
             p.join
         time.sleep(5)
-        print('alive = 'alive+'\n')
+        print('alive = '+alive+'\n')
         alive=list(alive)
         push(alive,outfile)
         cleanup(clash)
